f483f9567bd5680054a2a4f5049e04a97cd09103
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-29 18:54:42 -0400

    Agrega Hilt y permiso de internet

---
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/erick_estrada_ap2_p2App.kt
@@ -0,0 +1,12 @@
+package com.example.erick_estrada_ap2_p2
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+
+@HiltAndroidApp
+class erick_estrada_ap2_p2App : Application() {
+}
