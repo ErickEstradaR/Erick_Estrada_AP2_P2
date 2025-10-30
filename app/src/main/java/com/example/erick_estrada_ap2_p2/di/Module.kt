f4a62784b7c310c5310a17d01900060df91a815b
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-29 20:17:23 -0400

    Agrega Retrofit y OkHttp

--- app/src/main/java/com/example/erick_estrada_ap2_p2/di/Module.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/di/Module.kt
@@ -6,6 +6,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -19,4 +23,27 @@ object Module {
         Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
 }
