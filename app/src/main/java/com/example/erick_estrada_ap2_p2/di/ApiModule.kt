9ddf01d64e87d33144344b741f268798de14a5eb
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-29 20:04:01 -0400

    Agrega ApiModule con Moshi

---
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/di/ApiModule.kt
@@ -0,0 +1,22 @@
+package com.example.erick_estrada_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = ""
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+}
