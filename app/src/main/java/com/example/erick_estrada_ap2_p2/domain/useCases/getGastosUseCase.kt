7bc86daa3d1e5f6c289e3ba0490dec3f545dcf1a
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-05 19:58:47 -0400

    Añade funcion de getAllGastos

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastosUseCase.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastosUseCase.kt
@@ -3,4 +3,5 @@ package com.example.erick_estrada_ap2_p2.domain.useCases
 import com.example.erick_estrada_ap2_p2.domain.repository.GastosRepository
 
 class getGastosUseCase(private val repo : GastosRepository){
+    suspend operator fun invoke () = repo.getAllGastos()
 }
