154b743a6fa36cc55516c5647e1374fb2c7864bc
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-05 19:59:24 -0400

    Modifica repositorio de gastos

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/repository/GastosRepository.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/domain/repository/GastosRepository.kt
@@ -1,13 +1,12 @@
 package com.example.erick_estrada_ap2_p2.domain.repository
 
-import com.example.erick_estrada_ap2_p2.data.remote.GastosApi
 import com.example.erick_estrada_ap2_p2.domain.model.Gastos
 import kotlinx.coroutines.flow.Flow
 
 interface GastosRepository {
-    suspend fun getAllGastos ():List<GastosApi>
-    suspend fun getGastoById(gastoId: Int): GastosApi?
-    suspend fun saveGasto(gastos: Gastos): GastosApi
-    suspend fun deleteGasto(gastoId: Int): GastosApi
-    fun getAllFlow (): Flow<List<GastosApi>>
+    suspend fun getAllGastos ():List<Gastos>
+    suspend fun getGastoById(gastoId: Int): Gastos?
+    suspend fun saveGasto(gastos: Gastos)
+    suspend fun deleteGasto(gastoId: Int)
+    fun getAllFlow (): Flow<List<Gastos>>
 }
