d1d32492aba593f7b124af340a0a43e5f8ef66d7
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-06 12:42:00 -0400

    Agrega eventos para manejar la hoja de creación y eliminación

--- app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosEvent.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosEvent.kt
@@ -8,8 +8,12 @@ package com.example.erick_estrada_ap2_p2.presentation
     data class onNfcChange(val nfc : String):GastosEvent
     data class onItbisChange(val itbis : Double):GastosEvent
     data class onMontoChange(val monto: Double):GastosEvent
+    data class delete(val gastoId: Int) : GastosEvent
+
+    object ShowCreateSheet : GastosEvent
+    object HideCreateSheet : GastosEvent
 
     data object save:GastosEvent
     data object new:GastosEvent
-    data object delete:GastosEvent
+
 }
