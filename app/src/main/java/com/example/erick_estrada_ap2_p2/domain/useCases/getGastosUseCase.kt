edec0bfb92726971e008b80f780ae60657412198
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-06 12:41:26 -0400

    Actualiza getGastosUseCase para que use Flow

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastosUseCase.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastosUseCase.kt
@@ -1,7 +1,12 @@
 package com.example.erick_estrada_ap2_p2.domain.useCases
 
+import com.example.erick_estrada_ap2_p2.domain.model.Gastos
 import com.example.erick_estrada_ap2_p2.domain.repository.GastosRepository
+import kotlinx.coroutines.flow.Flow
 
-class getGastosUseCase(private val repo : GastosRepository){
-    suspend operator fun invoke () = repo.getAllGastos()
+class getGastosUseCase(  private val repository: GastosRepository
+) {
+    operator fun invoke(): Flow<List<Gastos>> {
+        return repository.getAllFlow()
+    }
 }
