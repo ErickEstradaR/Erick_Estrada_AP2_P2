d978b05e9075c6af6b143688ef3f5567f6a7817f
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-08 23:31:48 -0400

    Renombra Gastos a Gasto y corrige campos

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/model/Gasto.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/domain/model/Gasto.kt
@@ -1,10 +1,10 @@
 package com.example.erick_estrada_ap2_p2.domain.model
 
-data class Gastos (
+data class Gasto (
     val gastoId : Int,
     val fecha : String,
-    val suplidor : String,
-    val nfc : String,
+    val suplidor : String?,
+    val ncf : String?,
     val itbis : Double,
     val monto: Double,
 )
