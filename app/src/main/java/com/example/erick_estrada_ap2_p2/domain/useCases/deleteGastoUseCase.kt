408d65d8bc35b150d4847377b602cd01d1b07f39
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-08 23:34:29 -0400

    Elimina deleteGastoUseCase

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/deleteGastoUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.erick_estrada_ap2_p2.domain.useCases
-
-import com.example.erick_estrada_ap2_p2.domain.repository.GastosRepository
-
-class deleteGastoUseCase (private val repo : GastosRepository)
-{
-    suspend operator fun invoke(id: Int) = repo.deleteGasto(id)
-}
