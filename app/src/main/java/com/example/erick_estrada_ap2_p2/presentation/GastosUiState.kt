d8934b47d088138e5cd080bba46caa723c2bd458
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-08 23:34:19 -0400

    elimina companion object

--- app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosUiState.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosUiState.kt
@@ -1,10 +1,10 @@
 package com.example.erick_estrada_ap2_p2.presentation
 
-import com.example.erick_estrada_ap2_p2.domain.model.Gastos
+import com.example.erick_estrada_ap2_p2.domain.model.Gasto
 
 data class GastosUiState (
     val isLoading: Boolean = false,
-    val gastos: List<Gastos> = emptyList(),
+    val gastos: List<Gasto> = emptyList(),
     val userMessage: String? = null,
     val showCreateSheet: Boolean = false,
     val gastoId : Int = 0,
@@ -13,11 +13,7 @@ data class GastosUiState (
     val nfc: String = "",
     val itbis:Double =0.0,
     val monto:Double = 0.0
-    ) {
-        companion object {
-            fun default() = GastosUiState()
-        }
-    }
+    )
 
 
 
