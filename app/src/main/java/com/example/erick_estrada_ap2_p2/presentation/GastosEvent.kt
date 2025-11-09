2b263e7bfa77ff9c23f04c11570e90e140867877
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-09 14:50:07 -0400

    añade objetos y clases para eventos de la ui

--- app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosEvent.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/presentation/GastosEvent.kt
@@ -1,20 +1,25 @@
 package com.example.erick_estrada_ap2_p2.presentation
 
+import com.example.erick_estrada_ap2_p2.domain.model.Gasto
 
- sealed interface GastosEvent
+
+sealed interface GastosEvent
 {
+    data object cargar : GastosEvent
     data class onFechaChange(val fecha: String) : GastosEvent
     data class onSuplidorChange (val suplidor : String):GastosEvent
     data class onNcfChange(val ncf : String):GastosEvent
     data class onItbisChange(val itbis : Double):GastosEvent
     data class onMontoChange(val monto: Double):GastosEvent
+    data class obtener(val id: Int?): GastosEvent
+    object userMessageShown : GastosEvent
 
+    object showCreateSheet : GastosEvent
+    object hideCreateSheet : GastosEvent
 
-    object ShowCreateSheet : GastosEvent
-    object HideCreateSheet : GastosEvent
+    data class crear(val gasto: Gasto): GastosEvent
+    data class actualizar(val gasto: Gasto) : GastosEvent
 
-    data object crear:GastosEvent
-    data object actualizar:GastosEvent
     data object new:GastosEvent
 
 }
