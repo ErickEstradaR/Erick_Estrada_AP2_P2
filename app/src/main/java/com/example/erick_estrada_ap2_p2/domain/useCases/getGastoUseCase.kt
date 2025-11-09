5fa9cd8d97039bd54463bb4e5cb770c85a367d8a
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-08 23:33:14 -0400

    Corrige el nombre del método en `getGastoUseCase`

--- app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastoUseCase.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/domain/useCases/getGastoUseCase.kt
@@ -3,5 +3,5 @@ package com.example.erick_estrada_ap2_p2.domain.useCases
 import com.example.erick_estrada_ap2_p2.domain.repository.GastosRepository
 
 class getGastoUseCase (private val repo : GastosRepository) {
-    suspend operator fun invoke (id: Int) = repo.getGastoById(id)
+    suspend operator fun invoke (id: Int) = repo.getGasto(id)
 }
