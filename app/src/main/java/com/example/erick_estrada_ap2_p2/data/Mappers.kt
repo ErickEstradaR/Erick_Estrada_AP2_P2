c2efe9bdba44d9b4df107382577c0f5e72e5f5f7
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-09 20:37:33 -0400

    Agrega mapper

--- app/src/main/java/com/example/erick_estrada_ap2_p2/data/Mappers.kt
+++ app/src/main/java/com/example/erick_estrada_ap2_p2/data/Mappers.kt
@@ -1,10 +1,18 @@
 package com.example.erick_estrada_ap2_p2.data
 
-import com.example.erick_estrada_ap2_p2.data.remote.GastosDto
 import com.example.erick_estrada_ap2_p2.data.remote.dto.GastoResponse
 import com.example.erick_estrada_ap2_p2.domain.model.Gasto
 
 fun GastoResponse.toDomain() = Gasto(
+    gastoId = gastoId,
+    fecha = fecha,
+    suplidor = suplidor,
+    ncf = ncf,
+    itbis = itbis,
+    monto = monto,
+)
+
+fun Gasto.toDto() = GastoResponse(
     gastoId = gastoId,
     fecha = fecha,
     suplidor = suplidor,
